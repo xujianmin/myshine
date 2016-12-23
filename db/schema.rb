62391a2f7aa1efeff5adf4b195a394594dbf3530
Jimmy Xu <jianmin.xu@usingnow.com>
2016-12-23 18:52:46 +0800

    c3 add the index on first_name, last_name and email for higher search performane.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217053834) do
+ActiveRecord::Schema.define(version: 20161223102914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20161217053834) do
     t.string   "username",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index "lower((email)::text)", name: "customers_lower_email", using: :btree
+    t.index "lower((first_name)::text) varchar_pattern_ops", name: "customers_lower_first_name", using: :btree
+    t.index "lower((last_name)::text) varchar_pattern_ops", name: "customers_lower_last_name", using: :btree
     t.index ["email"], name: "index_customers_on_email", unique: true, using: :btree
     t.index ["username"], name: "index_customers_on_username", unique: true, using: :btree
   end
