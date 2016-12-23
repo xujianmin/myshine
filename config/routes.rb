b01d7a201e6a455728681837df6a9ead322e9e50
Jimmy Xu <jianmin.xu@usingnow.com>
2016-12-23 20:00:09 +0800

    c5 install angularjs and test it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'customers/index'
+  get 'angular_test/index'
 
-  get 'customer/index'
+  get 'customers/index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
