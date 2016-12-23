26b99de9c6c4bd0b721a6718211e08f659b6d389
Jimmy Xu <jianmin.xu@usingnow.com>
2016-12-23 19:32:32 +0800

    c4 add pagination to customer search result.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,9 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
+    @page = (params[:page] || 0).to_i
+
     if params[:keywords].present?
       @keywords = params[:keywords]
 
@@ -7,8 +11,9 @@ class CustomersController < ApplicationController
 
       @customers = Customer.where(
         customer_search_term.where_clause,
-        customer_search_term.where_args).order(
-          customer_search_term.order)
+        customer_search_term.where_args).
+        order(customer_search_term.order).
+        offset(PAGE_SIZE * @page).limit(PAGE_SIZE)
     else
       @customers = []
     end
